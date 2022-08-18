dca89f360838292315ab052084d479f1b1a397a5
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2022-08-18 10:32:58 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender JavaScript!!');
+todoList.nuevoTodo(tarea);
+
+tarea.completado = true;
+
+console.log(todoList);
+
+crearTodoHtml(tarea);
