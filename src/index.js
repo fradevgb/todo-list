ea2a88b67f615bd8f74460e58dce2c10d190c3bb
Frank Gaspar <frank.gasparfcytsis@gmail.com>
2022-08-23 11:28:32 -0400

    Add localStorage

--- src/index.js
+++ src/index.js
@@ -5,11 +5,21 @@ import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript!!');
-todoList.nuevoTodo(tarea);
+// const tarea = new Todo('Aprender JavaScript!!');
+// todoList.nuevoTodo(tarea);
 
 // tarea.completado = true;
 
-console.log(todoList);
+// console.log(todoList);
 
-crearTodoHtml(tarea);
+// crearTodoHtml(tarea);
+
+// localStorage.setItem('mi-key', 'ABC1234');
+
+// setTimeout(() => {
+//   localStorage.removeItem('mi-key');
+// }, 1500);
+
+console.log(todoList.todos);
+
+todoList.todos.forEach((todo) => crearTodoHtml(todo));
